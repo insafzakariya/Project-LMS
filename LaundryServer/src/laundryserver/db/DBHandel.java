ddee9dac67b175b4aba1b48232247c8d99ad54c9
zinsaf <insaf.zak@gmail.com>
2013-10-10 12:10:17 +0530

    DBHandel.java

---
+++ LaundryServer/src/laundryserver/db/DBHandel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laundryserver.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author insaf
+ */
+public class DBHandel {
+
+    public static int setData(Connection connection, String sql, Object[] data) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < data.length; i++) {
+            preparedStatement.setObject((i + 1), data[i]);
+
+        }
+        int res = preparedStatement.executeUpdate();
+
+        return res;
+
+    }
+    public static ResultSet getData(Connection connection,String string) throws SQLException{
+        Statement statement=connection.createStatement();
+        ResultSet resultSet=statement.executeQuery(string);
+        return resultSet;
+        
+    }
+}
